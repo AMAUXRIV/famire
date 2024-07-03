2ab977c9ad9132f8fe22dc5eea6080edefc0cb41
Napwoy <106542613+Napwoy@users.noreply.github.com>
2024-07-03 07:23:02 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,98 +1,85 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+<div class="container d-flex justify-content-center">
+    <div class="row auth-container">
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+            <img src="{{ asset('https://t4.ftcdn.net/jpg/05/88/53/93/360_F_588539338_uPcOEHviKdCnUk2yTcQhXc5uAzmBfNb1.jpg') }}" alt="Rendang" class="auth-image">
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="register-header text-center">REGISTER</div>
+                    <div class="text-center mb-4">Payakumbuh Kota Rendang</div>
+                    <form method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('nickname') ? ' has-error' : '' }}">
-                            <label for="nickname" class="col-md-4 control-label">{{ trans('user.nickname') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nickname" type="text" class="form-control" name="nickname" value="{{ old('nickname') }}" required autofocus>
-
-                                @if ($errors->has('nickname'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('nickname') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">{{ trans('user.name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">{{ trans('user.email') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('gender_id') ? ' has-error' : '' }}">
-                            <label for="gender_id" class="col-md-4 control-label">{{ trans('user.gender') }}</label>
-
-                            <div class="col-md-6">
-                                {!! FormField::radios('gender_id', [1 => trans('app.male'), 2 => trans('app.female')], ['label' => false]) !!}
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">{{ trans('auth.password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group">
+                            <label for="nickname">{{ trans('user.nickname') }}</label>
+                            <input id="nickname" type="text" class="form-control {{ $errors->has('nickname') ? 'is-invalid' : '' }}" name="nickname" value="{{ old('nickname') }}" required autofocus>
+                            @if ($errors->has('nickname'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('nickname') }}
+                                </div>
+                            @endif
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">{{ trans('auth.password_confirmation') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                            <label for="name">{{ trans('user.name') }}</label>
+                            <input id="name" type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name" value="{{ old('name') }}" required>
+                            @if ($errors->has('name'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('name') }}
+                                </div>
+                            @endif
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
+                            <label for="email">{{ trans('user.email') }}</label>
+                            <input id="email" type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                            @if ($errors->has('email'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="gender_id">{{ trans('user.gender') }}</label>
+                            <div>
+                                {!! FormField::radios('gender_id', [1 => trans('app.male'), 2 => trans('app.female')], ['label' => false, 'class' => 'form-check-input']) !!}
                             </div>
+                            @if ($errors->has('gender_id'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('gender_id') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">{{ trans('auth.password') }}</label>
+                            <input id="password" type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" required>
+                            @if ($errors->has('password'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('password') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm">{{ trans('auth.password_confirmation') }}</label>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        </div>
+
+                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                            <button type="submit" class="btn btn-primary auth-btn">
+                                Register
+                            </button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</div><br><br><br><br><br>
 @endsection
