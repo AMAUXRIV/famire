02bb9eaec45ce2d0f63ac2f331bd3a9b7b9a52b7
Napwoy <106542613+Napwoy@users.noreply.github.com>
2024-07-03 07:22:40 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading  text-center"><b>SELAMAT DATANG 👋</b></div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+<div class="container d-flex justify-content-center">
+    <div class="row auth-container">
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+            <img src="{{ asset('https://t4.ftcdn.net/jpg/05/88/53/93/360_F_588539338_uPcOEHviKdCnUk2yTcQhXc5uAzmBfNb1.jpg') }}" alt="Rendang" class="auth-image">
+        </div>
+        <div class="col-md-6">
+            <div class="login-header text-center">SELAMAT DATANG 👋</div>
+            <div class="text-center mb-4">Silahkan Login Menggunakan Akun Anda</div>
+            <form method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">{{ trans('auth.email') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                <div class="form-group">
+                    <label for="email">{{ trans('auth.email') }}</label>
+                    <input id="email" type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">{{ trans('auth.password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Ingat saya
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ trans('auth.login') }}
-                                </button>
-
-                                <a class="btn btn-link " href="{{ route('password.request') }}">
-                                    {{ trans('auth.forgot_password') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                    @endif
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <label for="password">{{ trans('auth.password') }}</label>
+                    <input id="password" type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" required>
+                    @if ($errors->has('password'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('password') }}
+                        </div>
+                    @endif
+                </div>
+
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="remember">Ingat saya</label>
+                </div>
+
+                <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                    <button type="submit" class="btn btn-success auth-btn">
+                        {{ trans('auth.login') }}
+                    </button>
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ trans('auth.forgot_password') }}
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
 </div>
